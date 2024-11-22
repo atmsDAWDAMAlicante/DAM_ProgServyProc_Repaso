e4bf88a7850d295e109b20d4ad440a681f0697ad
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-11-22 18:31:12 +0100

    Bloques sincronizados: clase perro

---
+++ Repaso01/src/BloquesSincronizados/BloquesSincronizados02_Perro.java
@@ -0,0 +1,121 @@
+package BloquesSincronizados;
+
+public class BloquesSincronizados02_Perro {
+    public static void main(String[] args) {
+
+        // Se crea una única cuenta cuyo saldo inicial es de X €
+
+        Cuenta2 c = new Cuenta2();
+        Perro p = new Perro();
+
+        // EL OBJETO C SERÁ EL MONITOR
+        // REPRESENTACIÓN: TRES PARTES ENTRY SET-OWNER (quién posee el monitor)-WAIT SET
+
+        // CUANDO UN HILO (HR) QUIERE EJECUTAR EL MÉTODO DEL OBJETO CUENTA
+        // 1- HR ENTRA EN ENTRY SET
+        // 2- EL OBJETO C LE DA PASO Y HR ENTRA EN OWNER-- AHORA HR "TIENE EL MONITOR" (ACQUIRE)
+        // 3- AHORA LLEGA HI A ENTRY SET Y EL MONITOR OBJETO MIRA A VER SI HAY ALGUN OBJETO EN OWNER
+        // 4- COMO ESTÁ HR EN OWNER C PONE A HI A ESPERAR. CUANDO HR TERMINA (RELEASE) LIBERA EL MONITOR
+        // 5- C PERMITE A HI ENTRAR A SER OWNER PASA A SER POSEEDOR DEL MONITOR
+
+
+        // AHORA SE SUPONE QUE NO SE PUEDE ACCEDER A LA CLASE
+        // HAY QUE IR A LAS LLAMADAS A LOS MÉTODOS DENTRO DE LAS CLASES DE LOS HILOS
+        // cuenta.reintegrar(100); y se modifica
+        //synchronized(OBJETO QUE SE DESEA QUE ACTÚE COMO MONITOR)
+        // CUALQUIER OBJETO PUEDE ACTUAR COMO MONITOR DE SUS PROPIOS MÉTODOS O DE OTROS OBJETOS
+        // EL OBJETO TIENE QUE SER EL MISMO PARA LOS MÉTODOS QUE SE QUIERE CONTROLAR
+        // EL OBJETO CUENTA CONTROLA LOS DOS MÉTODOS PERO TIENE QUE SER EL MISMO OBJETO CUENTA
+        /*
+            synchronized(cuenta){
+                cuenta.ingresar(100);
+            }
+        *
+         */
+
+        // Dos hilos uno para los ingresos y otro para los reintegros
+
+        HiloIngreso2 hi = new HiloIngreso2(c, p);
+        HiloReintegro2 hr = new HiloReintegro2(c, p);
+
+        Thread ti = new Thread(hi);
+        Thread tr = new Thread(hr);
+
+        ti.start();
+        tr.start();
+
+
+        // Espera a que los dos hilos terminen.
+        try {
+            ti.join();
+            tr.join();
+        } catch (InterruptedException ex){
+            System.out.println("La Interrupción de los hilos");
+        }
+
+        // No está implementada la sincronización entre hilos
+        // El resultado debería ser 0 pero a causa de las CONDICIONES DE CARRERA
+        // será un resultado diferente cada vez
+
+        System.out.println("El saldo final de la cuenta: " + c.getSaldo());
+
+    }
+}
+
+class Cuenta2 {
+    private double saldo;
+    public double getSaldo() {return saldo;}
+    // Método para realizar los ingresos
+    public synchronized void ingresar(double cantidad) {saldo += cantidad;}
+    // Método para realizar los reintegros
+    public synchronized void reintegrar(double cantidad) {saldo -= cantidad;}
+}
+
+class HiloIngreso2 implements Runnable {
+    private Cuenta2 cuenta;
+    private Perro perro;
+    public HiloIngreso2(Cuenta2 cuenta, Perro perro) {
+        this.cuenta = cuenta;
+        this.perro = perro;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            // Ingresa 1000 veces 100 €
+            //cuenta.ingresar(100);
+            //synchronized(cuenta){
+            synchronized(perro){
+                cuenta.ingresar(100);
+                System.out.println("Vuelta: In-"+ i +" / Saldo:" + cuenta.getSaldo());
+            }
+        }
+    }
+}
+
+class HiloReintegro2 implements Runnable {
+    private Cuenta2 cuenta;
+    private Perro perro;
+
+    public HiloReintegro2(Cuenta2 cuenta, Perro perro) {
+        this.cuenta = cuenta;
+        this.perro = perro;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            // Reintegra 1000 veces 100 €
+            //cuenta.reintegrar(100);
+            //synchronized(cuenta){
+            synchronized(cuenta){
+                cuenta.reintegrar(100);
+                System.out.println("Vuelta: Re-"+ i +" / Saldo:" + cuenta.getSaldo());
+            }
+        }
+    }
+}
+
+class Perro{
+// Esta clase funciona como monitor
+}
