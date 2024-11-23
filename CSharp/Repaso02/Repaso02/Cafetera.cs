5641031fefa41cce627508ee6efdd1adf0b0cdc0
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-11-23 19:00:19 +0100

    async y await en C#

---
+++ CSharp/Repaso02/Repaso02/Cafetera.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso02
+	{
+	class Cafetera
+		{
+		public static int espera = 2000;
+		static async Task Main(string[] args) // await se ejecuta dentro de un método async EL MAIN
+			{
+			// PARA HACER UN AWAIT EL MÉTODO TIENE QUE DEVOLVER UN TASK
+			Task tCafe = PrepararCafe();
+			Task tTostadas = PrepararTostada();
+			await tCafe; // await funciona con tareas Task se ejecutan los dos métodos asíncronamente
+			await tTostadas; // se le dice al hilo principal que espere a que finalicen las dos
+			Console.WriteLine("Desayuno finalizado"); // finalizadas las dos tareas se muestra el mensaje final
+			Console.ReadKey();
+			}
+		static async Task PrepararCafe()
+			{
+			Console.Write("CAFETERA: Preparando el Café");
+			Console.WriteLine("Cafetera: se acerca al fuego");
+			await Task.Delay(espera);
+			//Task.Delay(espera).Wait();
+			Console.WriteLine("Cafetera: sale el café");
+			await Task.Delay(espera);
+			//Task.Delay(espera).Wait();
+			Console.WriteLine("Cafetera: se echa en la taza");
+			await Task.Delay(espera);
+			//Task.Delay(espera).Wait();
+			Console.WriteLine("Cafetera: se pone azucar");
+			await Task.Delay(espera);
+			//Task.Delay(espera).Wait();
+			Console.WriteLine("Cafetera: FINALIZADO");
+
+			}
+
+		static async Task PrepararTostada()
+			{
+			Console.Write("TOSTADORA: Preparando las tostadas");
+			Console.WriteLine("Tostadora: Se corta el pan");
+			await Task.Delay(espera);
+			Console.WriteLine("Tostadora: Se acerca el pan al fuego");
+			await Task.Delay(espera);
+			Console.WriteLine("Tostadora: Se pone la tostada en el plato");
+			await Task.Delay(espera);
+			Console.WriteLine("Tostadora: Se le echa aceite al pan");
+			await Task.Delay(espera);
+			Console.WriteLine("Tostadora: FINALIZADO");
+			}
+		}
+	}
---
+++ CSharp/Repaso02/Repaso02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso02
+	{
+	internal class Program
+		{
+		static void Main2(string[] args)
+			{
+			Console.WriteLine("Vamos a hacer una espera de 3 segundos");
+			Task.Delay(1000).Wait();// crea una tarea asíncrona que esperará 1 segundo
+									// pero la ejecución del hilo principal continúa
+			// para que se produzca la espera hay q ue añadir una llamada .wait
+			// El método delay devuelve unun objeto Tarea Task que también tiene un método wait
+			// Wait espera a que se complete la ejecución del objeto Task
+			// No ejecutará otra tarea hasta que finalice la Task (que es Delay)
+			
+			
+			Console.WriteLine("Espe");
+			Console.ReadKey();
+			}
+		}
+	}
